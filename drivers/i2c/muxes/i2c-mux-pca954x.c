c8c4657c2fcea725273863055713fb13e38bf065
Matt Madison <mmadison@sensity.com>
2016-04-13 10:18:40 -0700

    i2c-mux-pca954x: add synchronization and bus recovery
    - Add a mutex to keep the channel selected for an entire transaction
    - Only remember last_chan when the selection operation is successful
    - Clear last_chan before unlocking mutex in deselect function
    - Implement bus recovery on the mux and call recovery on its parent
    
    Signed-off-by: Matt Madison <mmadison@sensity.com>

--- drivers/i2c/muxes/i2c-mux-pca954x.c
+++ drivers/i2c/muxes/i2c-mux-pca954x.c
@@ -61,7 +61,7 @@ enum pca_type {
 struct pca954x {
 	enum pca_type type;
 	struct i2c_adapter *virt_adaps[PCA954X_MAX_NCHANS];
-
+	struct mutex transaction_lock;
 	u8 last_chan;		/* last register value */
 };
 
@@ -72,6 +72,7 @@ struct chip_desc {
 		pca954x_ismux = 0,
 		pca954x_isswi
 	} muxtype;
+	enum pca_type type;
 };
 
 /* Provide specs for the PCA954x types we know about */
@@ -80,30 +81,51 @@ static const struct chip_desc chips[] = {
 		.nchans = 2,
 		.enable = 0x4,
 		.muxtype = pca954x_ismux,
+		.type = pca_9540,
 	},
 	[pca_9543] = {
 		.nchans = 2,
 		.muxtype = pca954x_isswi,
+		.type = pca_9543,
 	},
 	[pca_9544] = {
 		.nchans = 4,
 		.enable = 0x4,
 		.muxtype = pca954x_ismux,
+		.type = pca_9544,
 	},
 	[pca_9545] = {
 		.nchans = 4,
 		.muxtype = pca954x_isswi,
+		.type = pca_9545,
 	},
 	[pca_9547] = {
 		.nchans = 8,
 		.enable = 0x8,
 		.muxtype = pca954x_ismux,
+		.type = pca_9547,
 	},
 	[pca_9548] = {
 		.nchans = 8,
 		.muxtype = pca954x_isswi,
+		.type = pca_9548,
 	},
 };
+
+#ifdef CONFIG_OF
+static const struct of_device_id pca954x_of_match[] = {
+	{ .compatible = "nxp,pca9540", &chips[pca_9540] },
+	{ .compatible = "nxp,pca9542", &chips[pca_9542] },
+	{ .compatible = "nxp,pca9543", &chips[pca_9543] },
+	{ .compatible = "nxp,pca9544", &chips[pca_9544] },
+	{ .compatible = "nxp,pca9545", &chips[pca_9545] },
+	{ .compatible = "nxp,pca9546", &chips[pca_9545] },
+	{ .compatible = "nxp,pca9547", &chips[pca_9547] },
+	{ .compatible = "nxp,pca9548", &chips[pca_9547] },
+	{ },
+};
+MODULE_DEVICE_TABLE(of, pca954x_of_match);
+#endif
 
 static const struct i2c_device_id pca954x_id[] = {
 	{ "pca9540", pca_9540 },
@@ -117,6 +139,12 @@ static const struct i2c_device_id pca954x_id[] = {
 	{ }
 };
 MODULE_DEVICE_TABLE(i2c, pca954x_id);
+
+static int pca954x_recover(struct i2c_adapter *adap);
+
+static struct i2c_bus_recovery_info pca954x_recovery_info = {
+	.recover_bus = pca954x_recover,
+};
 
 /* Write to mux register. Don't use i2c_transfer()/i2c_smbus_xfer()
    for this as they will try to lock adapter a second time */
@@ -154,6 +182,8 @@ static int pca954x_select_chan(struct i2c_adapter *adap,
 	u8 regval;
 	int ret = 0;
 
+	mutex_lock(&data->transaction_lock);
+
 	/* we make switches look like muxes, not sure how to be smarter */
 	if (chip->muxtype == pca954x_ismux)
 		regval = chan | chip->enable;
@@ -163,25 +193,94 @@ static int pca954x_select_chan(struct i2c_adapter *adap,
 	/* Only select the channel if its different from the last channel */
 	if (data->last_chan != regval) {
 		ret = pca954x_reg_write(adap, client, regval);
-		data->last_chan = regval;
+		data->last_chan = (ret < 0 ? 0 : regval);
 	}
 
 	return ret;
+}
+
+static int pca954x_unlock_mux(struct i2c_adapter *adap,
+			      void *client, u32 chan)
+{
+	struct pca954x *data = i2c_get_clientdata(client);
+	mutex_unlock(&data->transaction_lock);
+	return 0;
 }
 
 static int pca954x_deselect_mux(struct i2c_adapter *adap,
 				void *client, u32 chan)
 {
 	struct pca954x *data = i2c_get_clientdata(client);
+	int ret;
 
-	/* Deselect active channel */
 	data->last_chan = 0;
-	return pca954x_reg_write(adap, client, data->last_chan);
+	ret = pca954x_reg_write(adap, client, data->last_chan);
+	pca954x_unlock_mux(adap, client, chan);
+	return ret;
 }
 
 /*
  * I2C init/probing/exit functions
  */
+
+#ifdef CONFIG_OF
+static int pca954x_probe_dt(struct i2c_client *client, struct pca954x_platform_data **pdatap)
+{
+	struct pca954x *data = i2c_get_clientdata(client);
+	struct pca954x_platform_data *pdata;
+	struct device_node *node = client->dev.of_node;
+	struct device_node *child;
+	const struct of_device_id *matched_id;
+	const struct chip_desc *chip;
+
+	if (node == NULL)
+		return -ENODEV;
+
+	matched_id = of_match_node(pca954x_of_match, node);
+	if (matched_id == NULL)
+		return -ENODEV;
+	chip = matched_id->data;
+	pdata = devm_kzalloc(&client->dev, (sizeof(struct pca954x_platform_data) +
+					    chip->nchans * sizeof(struct pca954x_platform_mode)),
+			     GFP_KERNEL);
+	if (pdata == NULL)
+		return -ENOMEM;
+	pdata->modes = (struct pca954x_platform_mode *)(pdata + 1);
+
+	for_each_available_child_of_node(node, child) {
+		u32 chan, adap_id;
+		if (!of_device_is_compatible(child, "nxp,pca954x-bus"))
+			continue;
+		if (of_property_read_u32(child, "reg", &chan) != 0)
+			continue;
+		if (chan >= chip->nchans) {
+			dev_err(&client->dev, "invalid mux channel %u at %s\n",
+				chan, of_node_full_name(child));
+			continue;
+		}
+		if (chan >= pdata->num_modes)
+			pdata->num_modes = chan + 1;
+		if (of_property_read_u32(child, "nr", &adap_id) == 0)
+			pdata->modes[chan].adap_id = adap_id;
+		pdata->modes[chan].deselect_on_exit =
+			of_property_read_bool(child, "nxp,deselect-on-exit");
+		dev_dbg(&client->dev, "DT: found reg=%d, nr=%d, deselect=%s\n", chan, adap_id,
+			(pdata->modes[chan].deselect_on_exit ? "yes" : "no"));
+	}
+
+	if (pdata->num_modes == 0) {
+		dev_err(&client->dev, "no busses found\n");
+		devm_kfree(&client->dev, pdata);
+		return -ENODEV;
+	}
+
+	*pdatap = pdata;
+	data->type = chip->type;
+
+	return 0;
+}
+#endif
+
 static int pca954x_probe(struct i2c_client *client,
 			 const struct i2c_device_id *id)
 {
@@ -203,6 +302,14 @@ static int pca954x_probe(struct i2c_client *client,
 
 	i2c_set_clientdata(client, data);
 
+#ifdef CONFIG_OF
+	ret = pca954x_probe_dt(client, &pdata);
+	if (ret < 0) {
+		dev_err(&client->dev, "DT parsing error\n");
+		return ret;
+	}
+#endif
+
 	/* Get the mux out of reset if a reset GPIO is specified. */
 	gpio = devm_gpiod_get_optional(&client->dev, "reset", GPIOD_OUT_LOW);
 	if (IS_ERR(gpio))
@@ -217,7 +324,10 @@ static int pca954x_probe(struct i2c_client *client,
 		return -ENODEV;
 	}
 
+#ifndef CONFIG_OF
 	data->type = id->driver_data;
+#endif
+	mutex_init(&data->transaction_lock);
 	data->last_chan = 0;		   /* force the first selection */
 
 	idle_disconnect_dt = of_node &&
@@ -244,7 +354,7 @@ static int pca954x_probe(struct i2c_client *client,
 			i2c_add_mux_adapter(adap, &client->dev, client,
 				force, num, class, pca954x_select_chan,
 				(idle_disconnect_pd || idle_disconnect_dt)
-					? pca954x_deselect_mux : NULL);
+					? pca954x_deselect_mux : pca954x_unlock_mux);
 
 		if (data->virt_adaps[num] == NULL) {
 			ret = -ENODEV;
@@ -253,6 +363,7 @@ static int pca954x_probe(struct i2c_client *client,
 				" %d as bus %d\n", num, force);
 			goto virt_reg_failed;
 		}
+		data->virt_adaps[num]->bus_recovery_info = &pca954x_recovery_info;
 	}
 
 	dev_info(&client->dev,
@@ -296,11 +407,29 @@ static int pca954x_resume(struct device *dev)
 
 static SIMPLE_DEV_PM_OPS(pca954x_pm, NULL, pca954x_resume);
 
+static int pca954x_recover(struct i2c_adapter *adap)
+{
+	struct i2c_client *client = i2c_mux_pdata(adap);
+	struct i2c_adapter *parent = to_i2c_adapter(client->dev.parent);
+	struct pca954x *data = i2c_get_clientdata(client);
+
+	if (parent != NULL) {
+		dev_info(&client->dev, "%s: invoking recovery on %s\n", __func__, parent->name);
+		i2c_recover_bus(parent);
+	}
+	dev_info(&client->dev, "%s: resetting last channel\n", __func__);
+	data->last_chan = 0;
+	return i2c_smbus_write_byte(client, 0);
+}
+
 static struct i2c_driver pca954x_driver = {
 	.driver		= {
 		.name	= "pca954x",
 		.pm	= &pca954x_pm,
 		.owner	= THIS_MODULE,
+#ifdef CONFIG_OF
+		.of_match_table = pca954x_of_match,
+#endif
 	},
 	.probe		= pca954x_probe,
 	.remove		= pca954x_remove,
